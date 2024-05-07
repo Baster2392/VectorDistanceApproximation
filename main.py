2032d27712d7cace351870d79271350b5565015b
wrwoj <w.r.wojtyna@gmail.com>
2024-05-07 23:46:28 +0200

    Dynamic Siamese Layers, Early Stop Mechanism, Grid Search Function

--- main.py
+++ main.py
@@ -1,49 +1,63 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import csv
+import itertools
 
 from siamese_model import SiameseNetwork
 import vector_generator as vg
 
+CSV_FILE_PATH = 'training_results.csv'
 
-def trening(input_dim, hidden_dim):
-    # Przygotowanie danych treningowych
+
+def trening(input_dim, hidden_dim, learning_rate, num_layers, patience=1000):
+    #patience is the number of epochs we wait before checking for early stop
+    loss_threshold = learning_rate/10
     n_samples = 32
     max_value = 1
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    # Utworzenie instancji modelu
-    model = SiameseNetwork(input_dim, hidden_dim).to(device)
-
-    # Funkcja straty i optymizator
+    model = SiameseNetwork(input_dim, hidden_dim, num_layers).to(device)
     criterion = nn.L1Loss().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=0.00001)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-    # Trening modelu
+    # Training loop
+    prev_losses = []
     epochs = 100000
     for epoch in range(epochs):
         x_train, y_train = vg.generate_sample_data(n_samples, 0, max_value, input_dim)
-
-        # Konwersja danych treningowych i testowych na typ Float
         x_train = torch.tensor(x_train, dtype=torch.float).to(device)
         y_train = torch.tensor(y_train, dtype=torch.float).to(device)
 
         optimizer.zero_grad()
         outputs = model(x_train)
         loss = criterion(outputs, y_train)
-        # print("Calculated by model:", outputs)
-        # print("Correct:", y_train)
         loss.backward()
         optimizer.step()
+        # best to keep commented when using grid search
+        # Print loss for each epoch
+        #print(f'Epoch [{epoch + 1}/{epochs}], Loss: {loss.item()}')
 
         if loss.item() < 0.01:
             break
 
-        print(f'Epoch [{epoch + 1}/{epochs}], Loss: {loss.item()}')
-        # print(f'Epoch [{epoch + 1}/{epochs}], {torch.norm(y_train - outputs).item()}')
 
-    return model
+
+
+        prev_loss = loss.item() if epoch > 0 else float('inf')
+        prev_losses.append(loss.item())
+        if epoch >= patience:
+            avg_loss_change = sum((prev_losses[i] - prev_losses[i - 1]) for i in range(1, patience)) / patience
+            if abs(avg_loss_change) < loss_threshold:
+                print(f'Converged Badly! Average loss change: {avg_loss_change}')
+                break
+            prev_losses.pop(0)
+
+       
+
+    return model, epoch+1, loss.item()
+
 
 
 def testing(model, n_samples, input_dim):
@@ -65,10 +79,38 @@ def testing(model, n_samples, input_dim):
             print(test_outputs[i], y_test[i])
 
 
+def grid_search(input_dim, hidden_dims, learning_rates, num_layers_list):
+    best_epoch = float('inf')
+    best_params = None
+
+    with open(CSV_FILE_PATH, mode='w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['Hidden Dim', 'Learning Rate', 'Num Layers', 'Epoch', 'Loss'])
+
+        for hidden_dim, learning_rate, num_layers in itertools.product(hidden_dims, learning_rates, num_layers_list):
+            model, epoch, loss = trening(input_dim, hidden_dim, learning_rate, num_layers)
+            writer.writerow([hidden_dim, learning_rate, num_layers, epoch, loss])
+            print(
+                f'Parameters: Hidden Dim={hidden_dim}, Learning Rate={learning_rate}, Num Layers={num_layers}, Epoch={epoch}, Loss={loss}')
+
+            if epoch < best_epoch:
+                best_epoch = epoch
+                best_params = {'hidden_dim': hidden_dim, 'learning_rate': learning_rate, 'num_layers': num_layers, 'loss':loss}
+# do not treat the best params as definitive, always consult with csv,
+# sometimes because of early stop mechanisms the best params cause bigger
+# loss then some other parameters
+    print(f'Best Parameters: {best_params}, Best Epoch: {best_epoch}')
+    return best_params
+
+
 if __name__ == '__main__':
     input_dim = 10
-    hidden_dim = 10
-    model = trening(input_dim, hidden_dim)
-    print("Testing model...")
-    testing(model, n_samples=20, input_dim=input_dim)
+    hidden_dims = [160, 170, 180, 200]
+    learning_rates = [0.0001, 0.001]
+    num_layers_list = [1, 2, 3]
 
+    best_params = grid_search(input_dim, hidden_dims, learning_rates, num_layers_list)
+
+
+    #hidden_dim = 1400
+    #model, epoch, loss = trening(input_dim, hidden_dim, 0.00001,4)
--- siamese_model.py
+++ siamese_model.py
@@ -3,17 +3,21 @@ import torch.nn as nn
 
 
 class SiameseNetwork(nn.Module):
-    def __init__(self, input_dim, hidden_dim):
+    def __init__(self, input_dim, hidden_dim, num_layers=4):
         super(SiameseNetwork, self).__init__()
 
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+
         # Neuron layers of model
         self.input_layer = ResizableInputLayer(input_dim)
-        self.siamese_layer1 = FullyConnectedLayer(input_dim, hidden_dim)
-        self.siamese_layer2 = FullyConnectedLayer(hidden_dim, hidden_dim)
-        self.siamese_layer3 = FullyConnectedLayer(hidden_dim, hidden_dim)
-        self.siamese_layer4 = FullyConnectedLayer(hidden_dim, hidden_dim)
+
+        # Create siamese layers dynamically
+        self.siamese_layers = nn.ModuleList([
+            FullyConnectedLayer(input_dim if i == 0 else hidden_dim, hidden_dim)
+            for i in range(num_layers)
+        ])
 
         # Compare vectors
         self.fc = torch.norm
@@ -23,20 +27,15 @@ class SiameseNetwork(nn.Module):
         x1 = self.input_layer.forward(x[:, 0, :])
         x2 = self.input_layer.forward(x[:, 1, :])
 
-        x1 = self.siamese_layer1.forward(x1)
-        x2 = self.siamese_layer1.forward(x2)
-
-
+        for siamese_layer in self.siamese_layers:
+            x1 = siamese_layer.forward(x1)
+            x2 = siamese_layer.forward(x2)
 
         out = torch.norm(x1 - x2, dim=1, keepdim=True)
         return out
 
     def scale_input_size(self, new_input_size):
-        self.input_layer = ResizableInputLayer(new_input_size, self.hidden_dim)
-
-    @staticmethod
-    def loss_function(y_true, y_pred):
-        return torch.norm(y_true - y_pred)
+        self.input_layer = ResizableInputLayer(new_input_size)
 
 
 class FullyConnectedLayer(nn.Module):
@@ -49,27 +48,6 @@ class FullyConnectedLayer(nn.Module):
         x = self.layer1(x)
         return x
 
-
-class Conv1DLayer(nn.Module):
-    def __init__(self, input_dim, hidden_dim):
-        super(Conv1DLayer, self).__init__()
-        self.layer1 = nn.Sequential(nn.Conv1d(input_dim, hidden_dim, kernel_size=3, padding=1))
-
-    def forward(self, x):
-        x = self.layer1(x)
-        return x
-
-
-class SiameseBatchNorm1dLayer(nn.Module):
-    def __init__(self, input_dim, hidden_dim):
-        super(SiameseBatchNorm1dLayer, self).__init__()
-        self.layer1 = nn.Sequential(nn.BatchNorm1d(input_dim))
-
-    def forward(self, x):
-        x = self.layer1(x)
-        return x
-
-
 class ResizableInputLayer(nn.Module):
     def __init__(self, input_size):
         super(ResizableInputLayer, self).__init__()
@@ -77,4 +55,3 @@ class ResizableInputLayer(nn.Module):
 
     def forward(self, x):
         return x
-
