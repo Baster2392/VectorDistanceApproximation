920ed4daceb8362f592ff53f9a57d478285b6edf
Kuba <kuba.slawinski.2003@gmail.com>
2024-06-18 18:50:38 +0200

    Updated training_recurrent.py and recurrect_model.py

--- models/recurrect_model.py
+++ models/recurrect_model.py
@@ -6,8 +6,37 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
 class SimpleRNN(nn.Module):
-    def __init__(self, input_dim, hidden_dim, num_layers_recurrent=1, num_layers_fc=2):
+    def __init__(self, input_dim, hidden_dim_r, hidden_dim_fc=64, num_layers_recurrent=1, num_layers_fc=2):
         super(SimpleRNN, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim_r = hidden_dim_r
+        self.hidden_dim_fc = hidden_dim_fc
+        self.num_layers_recurrent = num_layers_recurrent
+        self.num_layers_fc = num_layers_fc
+
+        self.rnn = nn.LSTM(2, hidden_dim_r, num_layers_recurrent, batch_first=True)
+
+        module_list = []
+        for i in range(self.num_layers_fc):
+            if i == 0:
+                module_list.append(nn.Linear(hidden_dim_r, hidden_dim_fc))
+            elif i != self.num_layers_fc - 1:
+                module_list.append(nn.Linear(hidden_dim_fc, hidden_dim_fc))
+            else:
+                module_list.append(nn.Linear(hidden_dim_fc, 1))
+        self.fc = nn.ModuleList(module_list)
+
+    def forward(self, input):
+        out, _ = self.rnn(input)
+        out = out[:, -1, :]
+        for layer in self.fc:
+            out = layer(out)
+        return out
+
+
+class ExperimentalRNN(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers_recurrent=1, num_layers_fc=2):
+        super(ExperimentalRNN, self).__init__()
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.num_layers_recurrent = num_layers_recurrent
@@ -15,12 +44,14 @@ class SimpleRNN(nn.Module):
 
         self.rnn = nn.LSTM(2, hidden_dim, num_layers_recurrent, batch_first=True)
 
+        inner_dim = 512
         module_list = []
-        for i in range(self.num_layers_fc):
-            if i != self.num_layers_fc - 1:
-                module_list.append(nn.Linear(hidden_dim, hidden_dim))
+        for i in range(self.num_layers_fc - 1):
+            if i == 0:
+                module_list.append(nn.Linear(hidden_dim, inner_dim))
             else:
-                module_list.append(nn.Linear(hidden_dim, 1))
+                module_list.append(nn.Linear(inner_dim, inner_dim))
+        module_list.append(nn.Linear(inner_dim, 1))
         self.fc = nn.ModuleList(module_list)
 
     def forward(self, input):
@@ -32,19 +63,20 @@ class SimpleRNN(nn.Module):
 
 
 def train(model, input_size, num_epochs=100):
-    n_samples = 32
+    n_samples = 64
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model.train().to(device)
 
     # Define loss function and optimizer
     criterion = nn.L1Loss().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
     min_lr = 1e-8
     patience = 300
     patience_after_min_lr = 1000
     loops_after_min_lr = 0
     factor = 0.5
     scheduler = ReduceLROnPlateau(optimizer, mode="min", patience=patience, factor=factor, verbose=True, min_lr=min_lr)
+    min_loss = float('inf')
 
     for epoch in range(num_epochs):
         x_data, y_data = vg.generate_sample_data_for_recurrent(n_samples, 0, 1, input_size)
@@ -67,8 +99,12 @@ def train(model, input_size, num_epochs=100):
         if loss.item() < 0.01:
             break
 
-        if (epoch + 1) % 10 == 0:
-            print(f'Epoch [{epoch + 1}], Loss: {loss.item()}, lr={optimizer.param_groups[0]["lr"]}')
+        if epoch % 1000 == 0:
+            print(f'Epoch [{epoch}], Loss: {loss.item()}, lr={optimizer.param_groups[0]["lr"]}')
+
+        if loss.item() < min_loss:
+            min_loss = loss.item()
+            print(f"Found new min_loss {min_loss} in epoch {epoch + 1}")
 
     return model
 
@@ -87,20 +123,20 @@ def validate(model, criterion, x_validate, y_validate):
 
 if __name__ == '__main__':
     # Example usage:
-    input_size = 25
-    hidden_size = 32
-    num_layers_recurrent = 1
+    input_size = 75
+    hidden_size = 128
+    num_layers_recurrent = 2
     num_layers_fc = 3
 
     # Initialize SiameseRNN model
-    model = SimpleRNN(input_size, hidden_size, num_layers_recurrent, num_layers_fc)
+    model = ExperimentalRNN(input_size, hidden_size, num_layers_recurrent, num_layers_fc)
 
     # Train model
     model = train(model, input_size, num_epochs=50000)
     torch.save(model.state_dict(), "../saved_models/siamese_recurrent_model.pt")
     criterion = nn.L1Loss().to("cuda" if torch.cuda.is_available() else "cpu")
 
-    x_data, y_data = vg.generate_sample_data_for_recurrent(32, 0, 1, input_size)
+    x_data, y_data = vg.generate_sample_data_for_recurrent(100, 0, 1, input_size)
     x_data, y_data = torch.tensor(x_data, dtype=torch.float), torch.tensor(y_data, dtype=torch.float)
 
     validate(model, criterion, x_data, y_data)
--- training_and_searching/training_recurrent.py
+++ training_and_searching/training_recurrent.py
@@ -11,6 +11,22 @@ import data_generators.vector_generator as vg
 CSV_FILE_PATH = '../results/grid_search.csv'
 
 
+def validate(model, criterion, vector_size):
+    model.eval()
+    x_validate, y_validate = vg.generate_sample_data_for_recurrent(100, 0, 1, vector_size, True)
+    x_validate = torch.tensor(x_validate, dtype=torch.float)
+    y_validate = torch.tensor(y_validate, dtype=torch.float)
+
+    with torch.no_grad():
+        y_pred = model(x_validate)
+        loss = criterion(y_pred, y_validate)
+    for i in range(len(y_validate)):
+        print("Predicted:", y_pred[i], "Actual:", y_validate[i])
+    print("Mean loss:", loss.item())
+    print("Max loss:", torch.max(abs(y_pred - y_validate)))
+    print("Min loss:", torch.min(abs(y_pred - y_validate)))
+    return loss.item()
+
 def train(model, criterion, optimizer, scheduler, epochs, n_samples,
           loss_tolerance=0.5, device=torch.device('cpu')):
     # Transfer components to device
@@ -39,7 +55,7 @@ def train(model, criterion, optimizer, scheduler, epochs, n_samples,
 
         # Print progress
         if epoch % 10 == 0:
-            print(f'Id: {model.input_dim}, Hd: {model.hidden_dim} Lrn: {model.num_layers_recurrent} Lfcn: {model.num_layers_fc} Epoch [{epoch}/{epochs}], Loss: {loss.item()}, Lr: {optimizer.param_groups[0]["lr"]}')
+            print(f'Id: {model.input_dim}, Hdr: {model.hidden_dim_r}, Hdf: {model.hidden_dim_fc}, Lrn: {model.num_layers_recurrent}, Lfcn: {model.num_layers_fc} Epoch [{epoch}/{epochs}], Loss: {loss.item()}, Lr: {optimizer.param_groups[0]["lr"]}')
 
         # Check if function converged
         if loss.item() < loss_tolerance:
@@ -57,16 +73,17 @@ def grid_search(criterion, optimizer_obj, scheduler_obj, epochs, n_samples, loss
     with open(path, mode='a', newline='') as csv_file:
         writer = csv.writer(csv_file)
 
-        for hidden_dim, num_layers_recurrent, num_layers_fc in itertools.product(hidden_dims, num_recurrent_layers_list, num_fc_layers_list):
-            model = SimpleRNN(input_dim, hidden_dim, num_layers_recurrent, num_layers_fc)
+        for hidden_dim, hidden_dim_fc, num_layers_recurrent, num_layers_fc in itertools.product(hidden_dims_r, hidden_dims_fc, num_recurrent_layers_list, num_fc_layers_list):
+            model = SimpleRNN(input_dim, hidden_dim, hidden_dim_fc=hidden_dim_fc, num_layers_recurrent=num_layers_recurrent, num_layers_fc=num_layers_fc)
             optimizer = optimizer_obj(model.parameters(), lr=0.001)
             if scheduler_obj is not None:
                 scheduler = scheduler_obj(optimizer, mode="min", patience=300, factor=0.75, verbose=True, min_lr=1e-8)
 
             model, epoch, loss, out_lr = train(model, criterion, optimizer, scheduler, epochs, n_samples, loss_tolerance, device)
-            writer.writerow([input_dim, hidden_dim // input_dim, hidden_dim, out_lr, num_layers_recurrent, epoch, loss])
+            validate_value = validate(model, criterion, input_dim)
+            writer.writerow([input_dim, hidden_dim // input_dim, hidden_dim, hidden_dim_fc, out_lr, num_layers_recurrent, num_layers_fc, epoch, loss, validate_value])
             print(
-                f'Parameters: Hidden Dim={hidden_dim}, Learning Rate={out_lr}, Num Layers={num_layers_recurrent}, Epoch={epoch}, Loss={loss}'
+                f'Parameters: Hidden Dim R={hidden_dim}, Learning Rate={out_lr}, Num Layers={num_layers_recurrent}, Epoch={epoch}, Loss={loss}'
             )
 
             if epoch < best_epoch:
@@ -83,18 +100,19 @@ def grid_search(criterion, optimizer_obj, scheduler_obj, epochs, n_samples, loss
 
 
 if __name__ == '__main__':
-    CSV_FILE_PATH = '../results/grid_search_recurrent_hidden_dim_32.csv'
+    CSV_FILE_PATH = '../results/searching_hidden_dim_r.csv'
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     criterion = nn.L1Loss()
     optimizer = optim.Adam
     scheduler = None
 
-    input_dims = [20]
+    input_dims = [50, 75, 100, 200]
     for input_dim in input_dims:
-        hidden_dims = [i for i in range(2, 34, 2)]
+        hidden_dims_r = [i for i in range(16, 257, 16)]
+        hidden_dims_fc = [64]
         learning_rates = [0.001]
         num_recurrent_layers_list = [2]
-        num_fc_layers_list = [2]
-        for i in range(3):
+        num_fc_layers_list = [3]
+        for i in range(1):
             print("Loop:", i, " for id=", input_dim)
-            best_params = grid_search(criterion, optimizer, scheduler, epochs=20000, n_samples=64, loss_tolerance=0.05, device=device)
+            best_params = grid_search(criterion, optimizer, scheduler, epochs=15000, n_samples=64, loss_tolerance=0.05, device=device)
