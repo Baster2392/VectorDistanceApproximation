f4fa839501622c439c11aa105ba543340b04fe4b
Kuba <kuba.slawinski.2003@gmail.com>
2024-06-20 17:45:23 +0200

    Added trained model 100_recurrent_1718897938.2512913.pth, dimensional_redction_model.py and reduced_vectors_generator.py

---
+++ data_generators/reduced_vectors_generator.py
@@ -0,0 +1,30 @@
+import numpy as np
+import torch
+import concurrent.futures
+from sklearn.decomposition import PCA
+import data_generators.vector_generator as vg
+
+pca = PCA(n_components=2)
+
+def reduce_pairs(pair):
+    pca.fit(pair)
+    reduced_pair = pca.transform(pair)
+    return reduced_pair
+
+
+def generate_sample_data(size, min_value, max_value, num_pairs):
+    pairs = vg.generate_vector_pairs(min_value, max_value, size, num_pairs)
+    reduced_pairs = []
+    for pair in pairs:
+        reduced_pairs.append(reduce_pairs(pair))
+    return pairs, np.asarray(reduced_pairs)
+
+
+if __name__ == '__main__':
+    x, y, = generate_sample_data(10, 0, 1, 5)
+    print(x)
+    print(y)
+    distance_o = np.linalg.norm(x[0, 0, :] - x[0, 1, :])
+    distance_r = np.linalg.norm(y[0, 0] - y[0, 1])
+    print(distance_o)
+    print(distance_r)
